8e3c4b69ac42faf81bfa0fbb5aedc168cbca2b3a
Hope Alemayehu <hopesp444@gmail.com>
2024-08-05 14:20:32 +0300

    Task 1 Completed

---
+++ go.mod
@@ -0,0 +1,5 @@
+module Task1
+
+go 1.22.5
+
+require github.com/stretchr/testify v1.9.0 // indirect
---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func calculateAverage(total float32, numberOfSubjects int) float32 {
+	ave := float32(total) / float32(numberOfSubjects)
+	return ave
+}
+
+func display(name string, subjectToGrade map[string]float32, Average float32) {
+	fmt.Println("Student Report Card")
+	fmt.Println("---------------------")
+	fmt.Println("Name:", name)
+	fmt.Println("---------------------")
+
+	fmt.Println("Subjects and Grades:")
+	fmt.Println("---------------------")
+	for key, value := range subjectToGrade {
+		fmt.Printf("%-15s: %3.2f\n", key, value)
+	}
+	fmt.Println("---------------------")
+
+	fmt.Printf("Average of All subjects: %.2f\n", Average)
+	fmt.Println("---------------------")
+}
+
+func validateName(name string) bool {
+	invalidChars := "!@#$%^&*()_-+={}[]:;|<>,.?/~`1234567890"
+	//names should always be letters
+	if len(name) == 0 || strings.ContainsAny(name, invalidChars) {
+		return false
+	}
+	return true
+}
+
+func validateSubjectName(subjectName string) bool {
+	//Subject name might include numbers. Example CS101
+	return len(subjectName) != 0
+}
+
+func validateNumberOfSubjects(num int) bool {
+	//no upper limit because it depends on the country/places
+	return num >= 0
+}
+
+func validateGrade(grade float32) bool {
+	if grade < 0 || grade > 100 {
+		return false
+	}
+	return true
+}
+
+func main() {
+	var name string
+	var numberOfSubjects int
+	var subject string
+	var grade float32
+	var totalGrade float32
+	subjectToGrade := make(map[string]float32)
+
+	fmt.Print("Enter Your first name: ")
+	fmt.Scanln(&name)
+
+	if !validateName(name) {
+		fmt.Println("Invalid name. Please Enter a name that contains letters and spaces.")
+		return
+	}
+
+	fmt.Print("How many subjects did you take: ")
+	fmt.Scanln(&numberOfSubjects)
+
+	if !validateNumberOfSubjects(numberOfSubjects) {
+		fmt.Println("Invalid input.Enter a positive number")
+		return
+	}
+
+	for i := 0; i < numberOfSubjects; i++ {
+		fmt.Print("Subject Name: ")
+		fmt.Scanln(&subject)
+
+		if !validateSubjectName(subject) {
+			fmt.Println("Invalid subject name.")
+			i-- //decrement i to ask the user again
+			continue
+		}
+
+		fmt.Printf("%s grade: ", subject)
+		fmt.Scanln(&grade)
+
+		if !validateGrade(float32(grade)) {
+			fmt.Println("Invalid grade. Please enter a grade between 0 and 100.")
+			i--
+			continue
+		}
+
+		subjectToGrade[subject] = grade
+		totalGrade += grade
+	}
+
+	average := calculateAverage(totalGrade, numberOfSubjects)
+
+	display(name, subjectToGrade, average)
+
+}
